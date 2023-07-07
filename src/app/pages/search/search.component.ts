d4800500ad0b619bc579b83753e2e4bedf7ba93d
Fevzi Atanoğlu <fevziatan2@gmail.com>
2023-07-07 12:32:34 +0300

    Search Page Update

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
   selector: 'app-search',
@@ -7,4 +8,85 @@ import { Component } from '@angular/core';
 })
 export class SearchComponent {
 
+  searchString: string = "";
+  searchMovies: any[] = [{
+    adult
+      :
+      false,
+    backdrop_path
+      :
+      "/hZkgoQYus5vegHoetLkCJzb17zJ.jpg",
+    id
+      :
+      550,
+    original_language
+      :
+      "en",
+    original_title
+      :
+      "Fight Club",
+    overview
+      :
+      "A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground \"fight clubs\" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion."
+   , popularity
+      :
+      68.13
+    ,poster_path
+      :
+      "/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
+    release_date
+      :
+      "1999-10-15",
+    title
+      :
+      "Fight Club",
+    video
+      :
+      false,
+    vote_average
+      :
+      8.435,
+    vote_count
+      :
+      26695
+  }
+
+  ];
+  isNoResult: boolean = false;
+
+  constructor(private dataService: MoviesService) {
+
+  }
+
+  ngOnInit() {
+    // this.search("Fight");
+  }
+
+
+  search(query: string) {
+    this.dataService.searchMovie(query).subscribe(data => {
+      this.searchMovies = data.results.slice(0, 5);
+      console.log(this.searchMovies);
+
+      if (this.searchMovies.length === 0) {
+        this.isNoResult = true;
+      } else {
+        this.isNoResult = false;
+      }
+
+    });
+  }
+
+  handleSearch() {
+
+    if (this.searchString.length > 1) {
+      this.search(this.searchString);
+    }
+
+    console.log(this.isNoResult)
+  }
+
+  setSearchString(query: string) {
+    this.searchString = query;
+  }
 }
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -5,35 +5,39 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MoviesService {
-  apiKey: string = "c9248c6c02240338f7a7a2b49f34cb77"
+  apiKey: string = "?api_key=c9248c6c02240338f7a7a2b49f34cb77"
   constructor(private http: HttpClient) { }
 
 
   getTopMovies() {
-    return this.http.get<any>("https://api.themoviedb.org/3/movie/popular?api_key=" + this.apiKey)
+    return this.http.get<any>("https://api.themoviedb.org/3/movie/popular" + this.apiKey)
   }
 
   getNowPlayingMovies() {
-    return this.http.get<any>("https://api.themoviedb.org/3/movie/now_playing?api_key=" + this.apiKey)
+    return this.http.get<any>("https://api.themoviedb.org/3/movie/now_playing" + this.apiKey)
   }
 
   getTopRatedgMovies() {
-    return this.http.get<any>("https://api.themoviedb.org/3/movie/top_rated?api_key=" + this.apiKey)
+    return this.http.get<any>("https://api.themoviedb.org/3/movie/top_rated" + this.apiKey)
   }
 
   getPopularMovies() {
-    return this.http.get<any>("https://api.themoviedb.org/3/movie/popular?api_key=" + this.apiKey)
+    return this.http.get<any>("https://api.themoviedb.org/3/movie/popular" + this.apiKey)
   }
 
   getMoviesByCategory(category: string){
-    return this.http.get<any>(`https://api.themoviedb.org/3/movie/${category}?api_key=` + this.apiKey)
+    return this.http.get<any>(`https://api.themoviedb.org/3/movie/${category}` + this.apiKey)
   }
 
   getDetailsById(movieId: string){
-    return this.http.get<any>(`https://api.themoviedb.org/3/movie/${movieId}language=en-US?api_key=` + this.apiKey)
+    return this.http.get<any>(`https://api.themoviedb.org/3/movie/${movieId}language=en-US` + this.apiKey)
   }
 
   getReviewById(movieId: string){
-    return this.http.get<any>(`https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=` + this.apiKey)
+    return this.http.get<any>(`https://api.themoviedb.org/3/movie/${movieId}/reviews` + this.apiKey)
+  }
+
+  searchMovie(query: string){
+    return this.http.get<any>("https://api.themoviedb.org/3/search/movie" + this.apiKey + "&query=" + query)
   }
 }
